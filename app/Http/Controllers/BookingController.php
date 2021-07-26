976e1a7c1389f16ef4a9c6f2b8f8d545f8581361
GrimmLck <rhmtofficial@gmail.com>
2021-07-26 18:56:22 +0700

    edit BookingController

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -10,7 +10,7 @@ use Carbon\Carbon;
 use DateTime;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use DB;
+use Illuminate\Support\Facades\DB;
 
 class BookingController extends Controller
 {
