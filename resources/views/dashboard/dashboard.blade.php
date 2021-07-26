679bf84329ad7190c67480a5fb63f601d713fd25
GrimmLck <rhmtofficial@gmail.com>
2021-07-26 15:54:38 +0700

    remove style

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -15,7 +15,7 @@
             <div class="count green">{{\App\Booking::where('active', 1)->count()}}</div>
             <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i></i>{{Carbon\Carbon::today()->format('D, d M Y')}}</span>
         </div>
-        <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count" style="">
+        <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
             <iframe src="http://free.timeanddate.com/clock/i6xgpsj4/n552/tlid38/fn3/fs16/fc444/tct/pct/ftb/tt0/ts1/tb4" frameborder="0" width="175" height="65" allowTransparency="true"></iframe>
       </div>
     </div>
