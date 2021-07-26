8619667d46f6c9e8a5b19d2ef5e3722d33679b84
GrimmLck <rhmtofficial@gmail.com>
2021-07-26 16:00:23 +0700

    fix header profile ur

--- app/Laporan.php
+++ app/Laporan.php
@@ -14,7 +14,7 @@ class Laporan extends Model
             ->join('users', 'bookings.id_user', '=', 'users.id')
             ->join('pelanggan', 'bookings.id_pelanggan', '=', 'pelanggan.id')
             // ->where('bookings.active', 1)
-            ->select('bookings.*', 'kamar.no_kamar as nomer_kamar', 'kamar.lantai as lantai_kamar', 'kamar.blok_id as nama_blok', 'kamar.tipe as tipe_kamar', 'kamar.harga as harga_kamar', 'kamar.fasilitas as fasilitas_kamar', 'users.fullname as operator', 'pelanggan.no_ktp as ktp_pelanggan', 'pelanggan.nama as nama_pelanggan', 'pelanggan.telp as telp_pelanggan', 'pelanggan.alamat as alamat_pelanggan', [DB::raw("DATE_FORMAT(created_at, %Y-%m) date_report"),DB::raw('YEAR(updated_at) year, MONTH(created_at) month')])
+            ->select('bookings.*', 'kamar.no_kamar as nomer_kamar', 'kamar.lantai as lantai_kamar', 'kamar.blok_id as nama_blok', 'kamar.tipe as tipe_kamar', 'kamar.harga as harga_kamar', 'kamar.fasilitas as fasilitas_kamar', 'users.fullname as operator', 'pelanggan.no_ktp as ktp_pelanggan', 'pelanggan.nama as nama_pelanggan', 'pelanggan.telp as telp_pelanggan', 'pelanggan.alamat as alamat_pelanggan', [DB::raw("DATE_FORMAT(created_at, '%Y-%m') date_report"),DB::raw('YEAR(updated_at) year, MONTH(created_at) month')])
             ->orderby('date_report', 'desc')
             ->get();
     }
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -7,15 +7,15 @@
     <ul class="nav navbar-nav navbar-right">
       <li class="">
         <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-        @if(Auth::user()->jenis_kelamin == "perempuan")
+          @if(Auth::user()->jenis_kelamin == "perempuan")
           <img src="{{url('assets/images/female.jpg')}}" alt="">{{ Auth::user()->fullname }}
-        @else
+          @else
           <img src="{{url('assets/images/male.jpg')}}" alt="">{{ Auth::user()->fullname }}
-        @endif
+          @endif
           <span class=" fa fa-angle-down"></span>
         </a>
         <ul class="dropdown-menu dropdown-usermenu pull-right">
-          <li><a href="public/admin/karyawan/profile/{{ Auth::user()->id }}"> Profile</a></li>
+          <li><a href="{{ url('/admin/karyawan/profile/')Auth::user()->id }}"> Profile</a></li>
           <li><a href="#" id="log-out" data-target="#ModalLogout"><i class="fa fa-sign-out pull-right"></i> Log Out</a>
           </li>
         </ul>
@@ -25,24 +25,26 @@
 </div>
 <div id="ModalLogout" class="modal fade text-danger" role="dialog">
   <div class="modal-dialog">
-  <!-- Modal content-->
-      <div class="modal-content" style="border-radius: 0px;">
-          <div class="modal-header bg-danger" style="padding: 0px 10px;">
-            <button type="button" class="close" data-dismiss="modal">&times;</button>
-            <p>Log Out</p>
-          </div>
-          <div class="modal-body" style="min-height: 80px;">
-            <div class="alert alert-primary" style="padding: 1px 5px;border-radius: 0px;">
-              <center> <p>Apakah anda yakin ingin keluar?</p></center>
-            </div>
-          </div>
-          <div class="modal-footer">
-            <a href="{{ route('logout') }}" class="btn btn-danger" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"> Log Out</a>
-            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-              @csrf
-            </form>
-            <button type="button" class="btn btn-success" data-dismiss="modal">Batal</button>
-          </div>
+    <!-- Modal content-->
+    <div class="modal-content" style="border-radius: 0px;">
+      <div class="modal-header bg-danger" style="padding: 0px 10px;">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <p>Log Out</p>
       </div>
+      <div class="modal-body" style="min-height: 80px;">
+        <div class="alert alert-primary" style="padding: 1px 5px;border-radius: 0px;">
+          <center>
+            <p>Apakah anda yakin ingin keluar?</p>
+          </center>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <a href="{{ route('logout') }}" class="btn btn-danger" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"> Log Out</a>
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+          @csrf
+        </form>
+        <button type="button" class="btn btn-success" data-dismiss="modal">Batal</button>
+      </div>
+    </div>
   </div>
 </div>
