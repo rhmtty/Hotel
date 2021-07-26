3bb9738e20d4b7c849ce755f6084557d9de3da4a
GrimmLck <rhmtofficial@gmail.com>
2021-07-26 16:55:09 +0700

    fix laporan booking

--- app/Booking.php
+++ app/Booking.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Booking extends Model
 {
@@ -33,12 +34,12 @@ class Booking extends Model
             ->first();
     }
 
-    // public function scopeLaporanBooking($query){
-    //     return $query->join('kamar', 'bookings.id_kamar', '=', 'kamar.id')
-    //         ->join('users', 'bookings.id_user', '=', 'users.id')
-    //         ->join('pelanggan', 'bookings.id_pelanggan', '=', 'pelanggan.id')
-    //         ->select([DB::raw("DATE_FORMAT(bookings.created_at, '%Y-%m') date_report"),DB::raw('YEAR(bookings.updated_at) year, MONTH(bookings.created_at) month'),'bookings.id', 'bookings.*', 'kamar.no_kamar as nomer_kamar', 'kamar.lantai as lantai_kamar', 'kamar.blok_id as nama_blok', 'kamar.tipe as tipe_kamar', 'kamar.harga as harga_kamar', 'kamar.fasilitas as fasilitas_kamar', 'users.fullname as operator', 'pelanggan.no_ktp as ktp_pelanggan', 'pelanggan.nama as nama_pelanggan', 'pelanggan.telp as telp_pelanggan', 'pelanggan.alamat as alamat_pelanggan'])
-    //         ->orderBy('date_report', 'desc')
-    //         ->get();
-    // }
+    public function scopeLaporanBooking($query){
+        return $query->join('kamar', 'bookings.id_kamar', '=', 'kamar.id')
+            ->join('users', 'bookings.id_user', '=', 'users.id')
+            ->join('pelanggan', 'bookings.id_pelanggan', '=', 'pelanggan.id')
+            ->select([DB::raw("DATE_FORMAT(bookings.created_at, '%Y-%m') date_report"),DB::raw('YEAR(bookings.updated_at) year, MONTH(bookings.created_at) month'),'bookings.id', 'bookings.*', 'kamar.no_kamar as nomer_kamar', 'kamar.lantai as lantai_kamar', 'kamar.blok_id as nama_blok', 'kamar.tipe as tipe_kamar', 'kamar.harga as harga_kamar', 'kamar.fasilitas as fasilitas_kamar', 'users.fullname as operator', 'pelanggan.no_ktp as ktp_pelanggan', 'pelanggan.nama as nama_pelanggan', 'pelanggan.telp as telp_pelanggan', 'pelanggan.alamat as alamat_pelanggan'])
+            ->orderBy('date_report', 'desc')
+            ->get();
+    }
 }
